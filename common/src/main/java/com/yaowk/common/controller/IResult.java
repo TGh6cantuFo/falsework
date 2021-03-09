9950f35228f4d7d72919d497131df8e66aaa9848
TGh6cantuFo <wenren20071121@126.com>
2021-03-09 23:19:18 +0800

    思考 controller规范  暂时提交

--- common/src/main/java/com/yaowk/common/controller/IResult.java
+++ common/src/main/java/com/yaowk/common/controller/IResult.java
@@ -4,5 +4,5 @@ package com.yaowk.common.controller;
  * @authc yaowk
  * 2017/6/27
  */
-public class BaseController extends VueController {
+public interface IResult {
 }
--- common/src/main/java/com/yaowk/common/controller/VueController.java
+++ common/src/main/java/com/yaowk/common/controller/VueController.java
@@ -1,8 +1,10 @@
 package com.yaowk.common.controller;
 
+import com.jfinal.core.Controller;
+
 /**
  * @authc yaowk
  * 2017/6/27
  */
-public class VueController extends BaseController {
+public class VueController extends Controller implements IResult {
 }
