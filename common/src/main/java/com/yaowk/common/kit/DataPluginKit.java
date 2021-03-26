7ef9ff491111f7527b18f8c1b487d5fe9a13e590
TGh6cantuFo <wenren20071121@126.com>
2021-03-26 17:34:43 +0800

    输入数据库连接参数

--- common/src/main/java/com/yaowk/common/kit/DataPluginKit.java
+++ common/src/main/java/com/yaowk/common/kit/DataPluginKit.java
@@ -2,6 +2,7 @@ package com.yaowk.common.kit;
 
 import com.jfinal.kit.Prop;
 import com.jfinal.kit.PropKit;
+import com.jfinal.log.Log;
 import com.jfinal.plugin.druid.DruidPlugin;
 
 import javax.sql.DataSource;
@@ -16,6 +17,9 @@ public class DataPluginKit {
 
     public static DruidPlugin getDruidPlugin() {
         Prop prop = PropKit.use("jdbc.properties");
+        Log.getLog("").info(prop.get("jdbcUrl"));
+        Log.getLog("").info(prop.get("user"));
+        Log.getLog("").info(prop.get("password"));
         DruidPlugin druidPlugin = new DruidPlugin(prop.get("jdbcUrl"), prop.get("user"), prop.get("password"), prop.get("driverClass"));
         return druidPlugin;
     }
